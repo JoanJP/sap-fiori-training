b22da2a94ac81d31362debcdfd1e7c02354c7d70
JoanJP <panggestu3214@gmail.com>
2025-05-13 16:58:24 +0700

    Little update

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,10 +1,23 @@
-sap.ui.define(["sap/ui/core/mvc/Controller"], (Controller) => {
-  "use strict";
+sap.ui.define(
+  ["sap/ui/core/mvc/Controller", "sap/m/MessageToast"],
+  (Controller, MessageToast) => {
+    "use strict";
 
-  return Controller.extend("ui5.walkthrough.controller.App", {
-    onShowButtonHello() {
-      // show a native JavaScript alert
-      alert("Hello, I'm a button!");
-    },
-  });
-});
+    return Controller.extend("ui5.walkthrough.controller.App", {
+      onShowButtonHello() {
+        // show a native JavaScript alert
+        alert("Hello, I'm a button!");
+      },
+      onShowButtonToast() {
+        MessageToast.show(
+          "Hello, This is a NEW Button!"
+          // Below is the customization
+          // {
+          //   my: "center top",
+          //   at: "center top",
+          // }
+        );
+      },
+    });
+  }
+);
