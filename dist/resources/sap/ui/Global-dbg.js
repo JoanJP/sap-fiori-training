cdaf6d08cf9d6b8d4e7cce0027af688e1702c1d6
JoanJP <panggestu3214@gmail.com>
2025-05-23 15:34:41 +0700

    updated dist

--- dist/resources/sap/ui/Global-dbg.js
+++ dist/resources/sap/ui/Global-dbg.js
@@ -61,7 +61,7 @@ sap.ui.define([
 		 */
 		version: "1.135.0",
 		// buildinfo.lastchange is deprecated and is therefore defaulted to empty string
-		buildinfo : { lastchange : "", buildtime : "20250521-1009" }
+		buildinfo : { lastchange : "", buildtime : "20250523-1501" }
 	};
 
 	/**
